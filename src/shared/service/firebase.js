3078cdd0346344a19e3fd6818730f47a7c5cce59
BT (Grem) Jamiel <brianthomasjamiel@gmail.com>
2022-08-14 15:34:15 -0700

    code chunk commit

---
+++ src/shared/service/firebase.js
@@ -0,0 +1,18 @@
+import * as firebase from 'firebase/app';
+import './firebase/firestore';
+import 'firebase/auth';
+
+try {
+  firebase.initializeApp({
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  });
+} catch (error) {
+  if (!/already exists/u.test(error.message)) {
+    console.error('Firebase admin initialization error', error.stack);
+  }
+}
