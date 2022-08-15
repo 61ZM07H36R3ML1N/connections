7fab3fd7379e5ba239f6e3c17c8751067f4b5767
BT (Grem) Jamiel <brianthomasjamiel@gmail.com>
2022-08-14 19:05:12 -0700

    input blocks

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,14 +1,17 @@
-import { useState } from 'react'; 
+import { useState } from 'react';
 import styles from './styles.module.css';
 
 export const Signup = () => {
+  const [email, setEmail] = useState('');
 
-const [email, setEmail] = useState('');
+  const [pw, setPW] = useState('');
+  const [verifypw, setVerifyPW] = useState('');
 
-  return <div className={StyleSheet.main}>
-    <input
-        type="email"
-        value={}
-    />
-  </div>;
+  return (
+    <div className={StyleSheet.main}>
+      <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
+      <input type="password" value={pw} onChange={e => setPW(e.target.value)} />
+      <input type="password" value={verifypw} onChange={e => setVerifyPW(e.target.value)} />
+    </div>
+  );
 };
