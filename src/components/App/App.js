4d3949f41b30aca97508210e2562e3d27271f407
BT (Grem) Jamiel <brianthomasjamiel@gmail.com>
2022-08-14 19:23:55 -0700

    switched from login to signup

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,11 @@
-import { Login } from '../Login';
+import { Login, Signup } from '../Signup';
 import styles from './styles.modules.css';
 
 export const App = () => {
   return (
     <div className={styles.main}>
       Hello Chello
-      <Login />
+      <Signup />
     </div>
   );
 };
