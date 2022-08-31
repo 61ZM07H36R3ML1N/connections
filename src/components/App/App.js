627c3042279d8ee358fec89b1a35d212e6444759
BT (Grem) Jamiel <brianthomasjamiel@gmail.com>
2022-08-30 21:43:27 -0700

    more logic code chunk

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,25 @@
-import styles from '../Signup/styles.module.css';
-import { Login } from '../Login';
+import styles from './styles.module.css';
+import { useAuth } from '../../hooks/useAuth';
+import { Route, Switch } from 'react-router-dom';
+import { Page, Login, CreatePage, Dashboard } from '../';
 
 export const App = () => {
+  const { isAuthed, authUser } = useAuth();
+
   return (
     <div className={styles.main}>
-      <Login />
+      {authUser === undefined ? (
+        <></>
+      ) : isAuthed ? (
+        <Switch>
+          <Route exact path={['/', '/dashboard']} component={Dashboard} />
+          <Route exact path={['/create-page', '/edit-page/:id']} component={CreatePage} />
+
+          <Route path="/page/:id" component={Page} />
+        </Switch>
+      ) : (
+        <Login />
+      )}
     </div>
   );
 };
