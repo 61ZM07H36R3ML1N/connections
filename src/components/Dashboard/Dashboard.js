ff273a511f20d8c722158a48c0157ef6da1fe468
BT (Grem) Jamiel <brianthomasjamiel@gmail.com>
2022-08-31 20:40:49 -0700

    code chunk

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,3 +1,11 @@
+import { useEffect } from 'react';
+import { usePages } from '../../hooks';
+import { useAuth } from '../hooks/authUser';
+
 export const Dashboard = () => {
+  const { authUser } = useAuth();
+  const pages = usePages(authUser?.id);
+
+  useEffect(() => {}, [pages]);
   return <>Dashboard</>;
 };
