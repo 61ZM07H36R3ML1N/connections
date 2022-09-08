59225acf6b5cb9f169ca9747aced2fc14c620e9c
BT (Grem) Jamiel <brianthomasjamiel@gmail.com>
2022-09-07 20:27:59 -0700

    code chunk

--- .history/src/components/Dashboard/Dashboard_20220831204014.js
+++ .history/src/components/Dashboard/Dashboard_20220831204014.js
@@ -1,19 +1,11 @@
 import { useEffect } from 'react';
 import { usePages } from '../../hooks';
 import { useAuth } from '../hooks/authUser';
-import styles from './styles.module.css';
 
 export const Dashboard = () => {
   const { authUser } = useAuth();
   const pages = usePages(authUser?.id);
 
   useEffect(() => {}, [pages]);
-  return (
-    <div className={styles.main}>
-      <h1>Pages: </h1>
-      {pages.map((p, index) => {
-        return <div key={index}>{p.name}</div>;
-      })}
-    </div>
-  );
+  return <>Dashboard</>;
 };
